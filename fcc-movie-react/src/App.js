9db781bbaeab20dbb5835c8368cf80a19cf89527
aciDrums7 <edoardo.carradori.96@gmail.com>
2023-10-01 22:13:21 +0000

    Succesfully returned movies from API + Removed duplicated files and updated the correct ones

--- fcc-movie-react/src/App.js
+++ fcc-movie-react/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg'
 import './App.css'
+import api from './api/axiosConfig'
+import { useState, useEffect } from 'react'
 
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
-    )
+    const [movies, setMovies] = useState()
+
+    const getMovies = async () => {
+        try {
+            const response = await api.get('/api/v1/movie/')
+            console.log(response.data)
+            setMovies(response.data)
+        } catch (exc) {
+            console.error(exc)
+        }
+    }
+
+    useEffect(() => {
+        getMovies()
+    }, [])
+
+    return <div className="App"></div>
 }
 
 export default App
